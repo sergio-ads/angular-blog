e44943c81bb40bcd6259d5841318cfb3babdd748
Sérgio Guimarães de Andrade <Srgio Guimares de Andrade>
2024-01-07 23:22:04 -0300

    Atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainTitleComponent } from './pages/home/components/main-title/main-title.component';
+import { MainMiniCardComponent } from './pages/home/components/main-mini-card/main-mini-card.component';
+import { MainFooterComponent } from './core/components/main-footer/main-footer.component';
+import { MainHeaderComponent } from './core/components/main-header/main-header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainCarouselComponent } from './pages/home/components/main-carousel/main-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTitleComponent,
+    MainMiniCardComponent,
+    MainFooterComponent,
+    MainHeaderComponent,
+    HomeComponent,
+    MainCarouselComponent
   ],
   imports: [
     BrowserModule,
