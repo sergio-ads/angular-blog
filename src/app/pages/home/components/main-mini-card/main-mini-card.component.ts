b32bb0c9506b4c2023fb6b544b693c23b4f6f842
Sérgio Guimarães de Andrade <Srgio Guimares de Andrade>
2024-01-07 23:22:37 -0300

    Atualizações

---
+++ src/app/pages/home/components/main-mini-card/main-mini-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-mini-card',
+  templateUrl: './main-mini-card.component.html',
+  styleUrls: ['./main-mini-card.component.css']
+})
+export class MainMiniCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
